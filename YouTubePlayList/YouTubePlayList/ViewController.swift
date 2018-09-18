03d25236c364b7165772c495ccc957fe6c15b514
niteshtak <niteshtak@outlook.com>
2018-09-19 01:17:45 +0800

    initialized project

---
+++ YouTubePlayList/YouTubePlayList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YouTubePlayList
+//
+//  Created by Nitesh Tak on 9/19/18.
+//  Copyright © 2018 nitesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	let APIKEY = "AIzaSyC9r7YRNBk2spsbzh98g6c21BvVjhbqKC4"
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
