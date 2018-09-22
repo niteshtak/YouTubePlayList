f771940d273de22d1ce9700becc2ce0826f6dd7d
niteshtak <niteshtak@outlook.com>
2018-09-23 01:59:34 +0800

    removed youtube playerView

--- YouTubePlayList/Pods/Target Support Files/Pods-YouTubePlayList/Pods-YouTubePlayList-frameworks.sh
+++ YouTubePlayList/Pods/Target Support Files/Pods-YouTubePlayList/Pods-YouTubePlayList-frameworks.sh
@@ -137,13 +137,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/youtube-ios-player-helper/youtube_ios_player_helper.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/youtube-ios-player-helper/youtube_ios_player_helper.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- YouTubePlayList/YouTubePlayList/ViewControllers/VideoPlayerVC.swift
+++ YouTubePlayList/YouTubePlayList/ViewControllers/VideoPlayerVC.swift
@@ -7,33 +7,35 @@
 //
 
 import UIKit
-import youtube_ios_player_helper
 
 class VideoPlayerVC: UIViewController {
 	
 	var videoId: String?
 	
+	@IBOutlet weak var webView: UIWebView8!
 	@IBOutlet weak var spinner: UIActivityIndicatorView!
-	@IBOutlet weak var playerView: YTPlayerView!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		self.title = "90 Seconds"
-		var playerVars = [String: Any]()
-		playerVars["playsinline"] = 1
-		self.playerView.delegate = self
-		if let videoId = self.videoId {
-			self.playerView.load(withVideoId: videoId, playerVars: playerVars)
-		}
+		self.loadURLData()
     }
+	
+	func loadURLData() {
+		guard let videoId = self.videoId else { return }
+		guard let url = URL(string: Constants.kVideoUrl+videoId) else  { return }
+		let responseObj = URLRequest(url: url)
+		self.webView.loadRequest(responseObj)
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 	}
 }
 
-extension VideoPlayerVC: YTPlayerViewDelegate {
-	func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
+extension VideoPlayerVC: UIWebViewDelegate {
+	
+	func webViewDidFinishLoad(_ webView: UIWebView) {
 		self.spinner.stopAnimating()
 		self.spinner.isHidden = true
 	}
